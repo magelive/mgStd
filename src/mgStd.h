4ba6e37b30e449f739977c145a31f75069b32a35
magelive <443132264@qq.com>
2016-12-19 20:48:23 +0800

    add mgStd.h

---
+++ src/mgStd.h
@@ -0,0 +1,25 @@
+#ifndef __MG_STD_H
+#define __MG_STD_H
+
+#include <mgbase64.h>
+#include <mgavltree.h>
+#include <mgsha384.h>
+#include <mgrbtree.h>
+#include <mglist.h>
+#include <mgprocess_rate.h>
+#include <mgsha512.h>
+#include <mgmd5.h>
+#include <mgsha1.h>
+#include <LiteZip.h>
+#include <mgStd.h>
+#include <mgcrc32.h>
+#include <mgfastsort.h>
+#include <mgsplaytree.h>
+#include <mghmac_md5.h>
+#include <mgMurmurhash.h>
+#include <LiteUnzip.h>
+#include <mgBstree.h>
+#include <mgtime.h>
+#include <mgsha256.h>
+
+#endif
